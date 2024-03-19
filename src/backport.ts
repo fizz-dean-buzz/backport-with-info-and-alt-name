955b8fc979bdbfe39e47c132532eaf9ff821351d
Dean Kuhne <dean.kuhne@tylertech.com>
2024-03-19 11:17:27 -0400

    prettifier attempt 2

--- src/backport.ts
+++ src/backport.ts
@@ -275,7 +275,7 @@ const backport = async ({
       body: originalBody ?? "",
       mergeCommitSha,
       number,
-      title
+      title,
     });
     const head = getHead({ base, number });
     const labels = getLabels({
